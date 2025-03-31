b02f09beaa164c81284f44dc3c0e2a5c356b6fbe
CamiloRodriguez11 <camilo.rodriguez11@usa.edu.co>
2025-03-31 17:41:45 -0500

    Update figura.cpp

--- figura.cpp
+++ figura.cpp
@@ -27,20 +27,27 @@ int main() {
     int tamaños[100];
     
     for (int i = 0; i < f; i++) {
-        cout << "¿Cuántos vértices tiene la cara " << i + 1 << "? ";
+        cout << "¿Cuántos vertices tiene la cara " << i + 1 << "?: de 1 a " << N << ": ";
         cin >> tamaños [i];
-
-        cout << "Ingrese los índices de los vértices para la cara " << i + 1 << " separados por espacios: ";
-        for (int j = 0; j < tamaños[i]; j++) {
-            cin >> caras[i][j];
+        if (tamaños [i] > N) {
+            cout << "vertices invalidos. " << endl;
+            i--; 
+            continue;
+        }else{
+            for (int j = 0; j < tamaños[i]; j++) {
+                cout << "Ingrese el índice del vértice " << j + 1 << ": de 1 a " << N << ": ";
+                cin >> caras[i][j];
+                if (caras[i][j] < 1 || caras[i][j] > N) {
+                    cout << "Índice inválido. El índice debe estar entre 1 y " << N << "." << endl;
+                    j--; 
+                }
+            }
         }
     }
-
     
     for (int i = 0; i < N; i++) {
         cout << "v " << tablero[i][0] << " " << tablero[i][1] << " " << tablero[i][2] << endl;
     }
-
    
     for (int i = 0; i < f; i++) {
         cout << "f ";
